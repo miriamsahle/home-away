eb48d10dfb1df2efdc0fc4c296c08893b180a2a5
Miriam-arch <124446711+Miriam-arch@users.noreply.github.com>
2024-11-22 23:48:42 +0100

    added Stripe funcionality

--- components/card/CountryFlagAndName.tsx
+++ components/card/CountryFlagAndName.tsx
@@ -1,3 +1,4 @@
+import Flag from "react-world-flags";
 import { findCountryByCode } from "@/utils/countries";
 
 function CountryFlagAndName({ countryCode }: { countryCode: string }) {
@@ -6,10 +7,13 @@ function CountryFlagAndName({ countryCode }: { countryCode: string }) {
     validCountry!.name.length > 20
       ? `${validCountry!.name.substring(0, 20)}...`
       : validCountry!.name;
+
   return (
     <span className="flex justify-between items-center gap-2 text-sm ">
-      {validCountry?.flag} {countryName}
+      <Flag code={countryCode} style={{ width: 20, height: 20 }} />
+      {countryName}
     </span>
   );
 }
+
 export default CountryFlagAndName;
--- utils/actions.ts
+++ utils/actions.ts
@@ -404,6 +404,15 @@ export const createBookingAction = async (prevState: {
 }) => {
   const user = await getAuthUser();
 
+  await db.booking.deleteMany({
+    where: {
+      profileId: user.id,
+      paymentStatus: false,
+    },
+  });
+
+  let bookingId: null | string = null;
+
   const { propertyId, checkIn, checkOut } = prevState;
   const property = await db.property.findUnique({
     where: { id: propertyId },
@@ -429,10 +438,11 @@ export const createBookingAction = async (prevState: {
         propertyId,
       },
     });
+    bookingId = booking.id;
   } catch (error) {
     return renderError(error);
   }
-  redirect("/bookings");
+  redirect(`/checkout?bookingId=${bookingId}`);
 };
 
 export const fetchBookings = async () => {
@@ -440,6 +450,7 @@ export const fetchBookings = async () => {
   const bookings = await db.booking.findMany({
     where: {
       profileId: user.id,
+      paymentStatus: true,
     },
     include: {
       property: {
@@ -494,6 +505,7 @@ export const fetchRentals = async () => {
       const totalNightsSum = await db.booking.aggregate({
         where: {
           propertyId: rental.id,
+          paymentStatus: true,
         },
         _sum: {
           totalNights: true,
@@ -503,6 +515,7 @@ export const fetchRentals = async () => {
       const orderTotalSum = await db.booking.aggregate({
         where: {
           propertyId: rental.id,
+          paymentStatus: true,
         },
         _sum: {
           orderTotal: true,
@@ -610,6 +623,7 @@ export const fetchReservations = async () => {
 
   const reservations = await db.booking.findMany({
     where: {
+      paymentStatus: true,
       property: {
         profileId: user.id,
       },
@@ -638,7 +652,11 @@ export const fetchStats = async () => {
 
   const usersCount = await db.profile.count();
   const propertiesCount = await db.property.count();
-  const bookingsCount = await db.booking.count();
+  const bookingsCount = await db.booking.count({
+    where: {
+      paymentStatus: true,
+    },
+  });
 
   return {
     usersCount,
@@ -655,6 +673,7 @@ export const fetchChartsData = async () => {
 
   const bookings = await db.booking.findMany({
     where: {
+      paymentStatus: true,
       createdAt: {
         gte: sixMonthsAgo,
       },
@@ -675,4 +694,31 @@ export const fetchChartsData = async () => {
     return total;
   }, [] as Array<{ date: string; count: number }>);
   return bookingsPerMonth;
+};
+
+export const fetchReservationStats = async () => {
+  const user = await getAuthUser();
+  const properties = await db.property.count({
+    where: {
+      profileId: user.id,
+    },
+  });
+
+  const totals = await db.booking.aggregate({
+    _sum: {
+      orderTotal: true,
+      totalNights: true,
+    },
+    where: {
+      property: {
+        profileId: user.id,
+      },
+    },
+  });
+
+  return {
+    properties,
+    nights: totals._sum.totalNights || 0,
+    amount: totals._sum.orderTotal || 0,
+  };
 };
